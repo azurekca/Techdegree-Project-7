da2c5158f3b27d52356851804b3958b607fd8f64
Agata Zurek <agata.w.zurek@gmail.com>
2020-06-07 00:16:48 -0700

    Housekeeping

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,8 +1,5 @@
 import React from 'react';
 
-// https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-// https://farm66.staticflickr.com/65535/49979146261_757a6318b5.jpg  <-- this works
-
 const Photo = ({photo}) => (
   <li>
     <img 
