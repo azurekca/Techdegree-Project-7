be6547be313ac3d416194fcb86656a8a026a334a
Agata Zurek <agata.w.zurek@gmail.com>
2020-06-08 13:52:55 -0700

    Updates

---
+++ precache-manifest.32ba8bc177239aedbf4f465d30944444.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a089c9f958f33e9e56fe40ee7ed9fa0",
+    "url": "/react-gallery/index.html"
+  },
+  {
+    "revision": "03ab72f5c7555965161b",
+    "url": "/react-gallery/static/css/main.c507937e.chunk.css"
+  },
+  {
+    "revision": "dc1da199056fb9daab7b",
+    "url": "/react-gallery/static/js/2.c8e9c5d6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gallery/static/js/2.c8e9c5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ab72f5c7555965161b",
+    "url": "/react-gallery/static/js/main.feedfc65.chunk.js"
+  },
+  {
+    "revision": "606c4d89be82819a8889",
+    "url": "/react-gallery/static/js/runtime-main.e9beeb21.js"
+  }
+]);
