e9230d70e9444852b65123bff6b2005ce7bb1491
Agata Zurek <agata.w.zurek@gmail.com>
2020-06-08 18:33:42 -0700

    bug fix for HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter, Route, Switch, HashRouter } from 'react-router-dom';
+import { Route, Switch, HashRouter } from 'react-router-dom';
 import API_KEY from './config';
 
 // React Components
